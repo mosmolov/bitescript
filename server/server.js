e4d54aa005933e6e89c5b3227a893a1788855838
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2024-09-15 11:14:26 -0400

    set up mongoose

--- server/server.js
+++ server/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
